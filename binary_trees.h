65b70189cc7ab08e73bb7bccfc3f8031f54f9927
mlle-Kwamboka <99282148+mlle-Kwamboka@users.noreply.github.com>
2022-08-24 00:38:32 +0300

    Update binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -1,5 +1,14 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <math.h>
+#include <limits.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -17,5 +26,11 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
+
+void binary_tree_print(const binary_tree_t *);
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 #endif
